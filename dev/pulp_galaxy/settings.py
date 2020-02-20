2cc227404238a332d57d030e29ec5fd6da30d00d
Alexander Saprykin <cutwatercore@gmail.com>
2020-02-20 14:56:43 +0100

    WIP

--- dev/pulp_galaxy/settings.py
+++ dev/pulp_galaxy/settings.py
@@ -7,11 +7,11 @@ CONTENT_ORIGIN = "http://loclahost:8000"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pulp',
+        'NAME': os.environ.get('PULP_DB_NAME', ''),
         'HOST': os.environ.get('PULP_DB_HOST', 'localhost'),
         'PORT': os.environ.get('PULP_DB_PORT', ''),
-        'USER': 'pulp',
-        'PASSWORD': 'secret',
+        'USER': os.environ.get('PULP_DB_USER', ''),
+        'PASSWORD': os.environ.get('PULP_DB_PASSWORD', ''),
     }
 }
 
